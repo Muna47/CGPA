b1cd09b9b3a77c7bfc4d546bed95f8d8f1eef19f
Muna47 <mymunai552@gmail.com>
2024-09-15 13:54:42 +0600

    login class

---
+++ Login.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    JButton login, reg;
+
+    Login() {
+        setTitle("CGPA Forecast");
+        setSize(889, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        ImageIcon logo = new ImageIcon("C:\\Users\\HP\\Downloads\\_ੈ✧‧₊˚༄.jpeg");
+        setIconImage(logo.getImage());
+
+        ImageIcon bgImage = new ImageIcon("C:\\Users\\HP\\Pictures\\login.jpg");
+        JPanel bgPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        bgPanel.setLayout(null);
+
+        Main main = new Main();
+        login = main.button_func("Login", 365, 205, 140, 50, 123, 134, 238);
+        login.addActionListener(this);
+        bgPanel.add(login);
+
+        reg = main.button_func("Registration", 355, 265, 160, 50, 123, 134, 238);
+        reg.addActionListener(this);
+        bgPanel.add(reg);
+
+        setContentPane(bgPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == login) {
+            dispose();
+            SwingUtilities.invokeLater(() -> new Login_info().setVisible(true));
+        }
+        if (e.getSource() == reg) {
+            dispose();
+            SwingUtilities.invokeLater(() -> new Reg_info().setVisible(true));
+        }
+    }
+}
