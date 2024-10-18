b985fe8e15bb7692c89045f3da1603af033f322d
Muna47 <mymunai552@gmail.com>
2024-10-18 21:22:45 +0600

    connecting to database

---
+++ DatabaseConnection.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/cgpa";
+    private static final String username = "root";
+    private static final String password = "mysql";
+
+    public static Connection getconnection(){
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+}
